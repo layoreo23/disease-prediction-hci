cde666dcd1eb0368cbb1ea5869ac2616a4412eb5
VoidVampire <yashdsingh2004@gmail.com>
2024-03-24 04:57:19 +0530

    updated details page added symptoms list for input

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 12 19:21:39 2023
-
-@author: hp
-"""
-
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
@@ -12,13 +5,26 @@ import pickle
 model = pickle.load(open('model.pkl','rb'))
 app = Flask(__name__)
 
+symptoms = ['itching', 'continuous_sneezing', 'shivering', 'joint_pain',
+       'stomach_pain', 'vomiting', 'fatigue', 'weight_loss', 'restlessness',
+       'lethargy', 'high_fever', 'headache', 'dark_urine', 'nausea',
+       'pain_behind_the_eyes', 'constipation', 'abdominal_pain', 'diarrhoea',
+       'mild_fever', 'yellowing_of_eyes', 'malaise', 'phlegm', 'congestion',
+       'chest_pain', 'fast_heart_rate', 'neck_pain', 'dizziness',
+       'puffy_face_and_eyes', 'knee_pain', 'muscle_weakness',
+       'passage_of_gases', 'irritability', 'muscle_pain', 'belly_pain',
+       'abnormal_menstruation', 'increased_appetite', 'lack_of_concentration',
+       'visual_disturbances', 'receiving_blood_transfusion', 'coma',
+       'history_of_alcohol_consumption', 'blood_in_sputum', 'palpitations',
+       'inflammatory_nails', 'yellow_crust_ooze']
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', word="Home")
 
 @app.route('/details')
 def pred():
-    return render_template('details.html')
+    return render_template('details.html', word="Details", symptoms=symptoms)
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
@@ -46,7 +52,7 @@ def predict():
         b=b.reshape(1,45)
         prediction = model.predict(b)
         prediction = prediction[0]
-    return render_template('results.html', prediction_text="The probable diagnosis says it could be {}".format(prediction))
+    return render_template('results.html', word="Prediction", prediction_text="The probable diagnosis says it could be {}".format(prediction))
 
 
 if __name__ == "__main__":
